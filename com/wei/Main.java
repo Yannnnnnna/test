8ae19e03a5fc0265cb111e2547c1b06c8285afe9
Yannnnnnna <wyr0773@outlook.com>
2025-05-08 19:19:25 +0800

    修改项目

--- com/wei/Main.java
+++ com/wei/Main.java
@@ -6,7 +6,7 @@ package com.wei;
 public class Main {
     public static void main(String[] args) {
         System.out.println(123);
-        System.out.println(456);
+        System.out.println(123);
         System.out.println(789);
     }
 }
