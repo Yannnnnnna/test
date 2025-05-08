90125bc42338aee21329af848e04706403669108
Yannnnnnna <wyr0773@outlook.com>
2025-05-08 19:17:39 +0800

    新分支提交

--- com/wei/Main.java
+++ com/wei/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(456);
-        System.out.println(789);
+        System.out.println(8888);
     }
 }
